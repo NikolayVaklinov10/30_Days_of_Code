28cadcaa542528fb2d4d322fbeb2dd39f907b377
Nick <nikolayvaklinov@yahoo.com>
2019-11-16 23:58:59 +0000

    finding the total bill cost

---
+++ Day_2:Operators.py
@@ -0,0 +1,10 @@
+import sys
+
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
+    tip_percent = int(input().strip())
+    tax_percent = int(input().strip())
+    tip_amount = meal_cost * tip_percent / 100
+    tax_amout = meal_cost * tax_percent / 100
+    total_cost = round(meal_cost + tip_amount + tax_amout)
+    
