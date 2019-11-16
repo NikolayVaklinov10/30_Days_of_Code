4b0d5062e56f2ce74b0cefda4ba1a6e6925a643e
Nick <nikolayvaklinov@yahoo.com>
2019-11-16 22:33:08 +0000

    day one commit

---
+++ Day_1:Data_Types.py
@@ -0,0 +1,11 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+input_int = int(input())
+input_double = float(input())
+input_string = str(input())
+# Read and save an integer, double, and String to your variables.
+print(i+input_int)
+print(round(d+input_double))
+print(s+input_string)
