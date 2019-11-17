d876f66d688bbdf14e5b2b5f587c6c4dff415b7b
Nick <nikolayvaklinov@yahoo.com>
2019-11-17 16:29:36 +0000

    splitting a string equally

---
+++ Day_6:Let's_Review.py
@@ -0,0 +1,17 @@
+
+num_test_case = int(input())
+
+for i in range(num_test_case):
+    test_string = input()
+    even_indexed_characters = ''
+    odd_indexed_characters = ''
+
+    for j in range(len(test_string)):
+        if j % 2 == 0:
+            even_indexed_characters += test_string[j]
+        else:
+            odd_indexed_characters += test_string[j]
+
+    print('{} {}'.format(even_indexed_characters, odd_indexed_characters))
+
+
